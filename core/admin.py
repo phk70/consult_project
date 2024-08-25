ccc9782474e830166a35035f5d47160864ba9180
koles <yg@htfv.com>
2024-08-25 11:56:56 +0700

    повторно

--- core/admin.py
+++ core/admin.py
@@ -1,2 +1,16 @@
 from django.contrib import admin
+from .models import Master, Service
 
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('services',)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price',)
+    search_fields = ('name',)
+    filter_horizontal = ('name',)
