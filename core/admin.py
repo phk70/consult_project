3b2a180d35ef88cd38da1967b690305381889898
koles <yg@htfv.com>
2024-08-25 13:27:42 +0700

    Сделаны Миграции, добавил шаблоны Base, Thanks, Main

--- core/admin.py
+++ core/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
 from .models import Master, Service, Client
 
+admin.site.register(Master)
+admin.site.register(Service)
+admin.site.register(Client)
 
-@admin.register(Master)
-class MasterAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'contact_info')
-    search_fields = ('first_name', 'last_name')
-    list_filter = ('services',)
+# @admin.register(Master)
+# class MasterAdmin(admin.ModelAdmin):
+#     list_display = ("first_name", "last_name", "contact_info")
+#     search_fields = ("first_name", "last_name")
+#     list_filter = ("services",)
 
 
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price',)
-    search_fields = ('name',)
-    filter_horizontal = ('name',)
+# @admin.register(Service)
+# class ServiceAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "name",
+#         "price",
+#     )
+#     search_fields = ("name",)
+#     filter_horizontal = ("name",)
 
-@admin.register(Service)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone',)
-    search_fields = ('name',)
-    filter_horizontal = ('name',)
+
+# @admin.register(Service)
+# class ClientAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "name",
+#         "phone",
+#     )
+#     search_fields = ("name",)
+#     filter_horizontal = ("name",)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,18 @@ from tabnanny import verbose
 from django.db import models
 
 
+class Service(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Услугу"
+        verbose_name_plural = "Услуги"
+
+
 class Master(models.Model):
     first_name = models.CharField(max_length=30, verbose_name="Имя мастера")
     last_name = models.CharField(max_length=40, verbose_name="Фамилия мастера")
@@ -9,30 +21,14 @@ class Master(models.Model):
     photo = models.ImageField(
         blank=True, null=True, upload_to="masters/photos/", verbose_name="Фото мастера"
     )
-    services = models.ManyToManyField(
-        Service, related_name="name", verbose_name="Услуги"
-    )
+    services = models.ManyToManyField(Service, related_name="masters")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
     class Meta:
-        verbose_name = "Мастер"
+        verbose_name = "Мастера"
         verbose_name_plural = "Мастера"
-
-
-class Service(models.Model):
-    name = models.CharField(max_length=200, verbose_name="Название услуги")
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="Стоимость"
-    )
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Услуга"
-        verbose_name_plural = "Услуги"
 
 
 class Client(models.Model):
@@ -40,9 +36,7 @@ class Client(models.Model):
     phone = models.DecimalField(
         max_digits=10, decimal_places=2, verbose_name="Телефон клиента"
     )
-    master = models.ForeignKey(
-        Master, related_name="first_name", on_delete=models.CASCADE
-    )
+    master = models.ForeignKey(Master, related_name="master", on_delete=models.CASCADE)
     service = models.ForeignKey(
         Service, related_name="services", on_delete=models.CASCADE
     )
@@ -51,5 +45,5 @@ class Client(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Клиент"
+        verbose_name = "Клиента"
         verbose_name_plural = "Клиенты"
